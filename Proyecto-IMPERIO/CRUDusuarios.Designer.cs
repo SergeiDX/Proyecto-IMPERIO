fe0ef700b0a9f460730429d3e2fc599297534268
SERGIO SALAZAR ZUNIGA <sergio_salazar@uadec.edu.mx>
2023-09-01 21:26:09 -0600

    Proyecto actualizacion #0.0.1

---
+++ Proyecto-IMPERIO/CRUDusuarios.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Proyecto_IMPERIO
+{
+    partial class CRUDusuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtusuario = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuarios";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(140, 57);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(361, 22);
+            this.txtnombre.TabIndex = 10;
+            // 
+            // txtusuario
+            // 
+            this.txtusuario.Location = new System.Drawing.Point(140, 95);
+            this.txtusuario.Name = "txtusuario";
+            this.txtusuario.Size = new System.Drawing.Size(361, 22);
+            this.txtusuario.TabIndex = 20;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(61, 98);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(54, 16);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Usuario";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(140, 132);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '•';
+            this.txtpass.Size = new System.Drawing.Size(361, 22);
+            this.txtpass.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(41, 135);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(76, 16);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "Contraseña";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(29, 195);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(126, 30);
+            this.btnAgregar.TabIndex = 23;
+            this.btnAgregar.Text = "Agregar Usuario";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(205, 195);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(137, 30);
+            this.btnActualizar.TabIndex = 24;
+            this.btnActualizar.Text = "Actualizar Usuario";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(364, 195);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(137, 30);
+            this.btnBorrar.TabIndex = 25;
+            this.btnBorrar.Text = "Borrar Usuario";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // CRUDusuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 287);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtusuario);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CRUDusuarios";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUDusuarios";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.CRUDusuarios_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
---
+++ Proyecto-IMPERIO/CRUDusuarios.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_IMPERIO
+{
+    public partial class CRUDusuarios : Form
+    {
+        SQLControl SQLControl = new SQLControl();
+        public CRUDusuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void CRUDusuarios_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Close();
+        }
+        public void AgregarUsuario()
+        {
+            SQLControl.cnn.Open();
+            SqlCommand cmd = new SqlCommand("spAgregarUsuario", SQLControl.cnn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@nombre", txtnombre.Text);
+            cmd.Parameters.AddWithValue("@usuario", txtusuario.Text);
+            cmd.Parameters.AddWithValue("@pass", txtpass.Text);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Usuario agregado correctamente.");
+            }
+            catch (SqlException EX)
+            {
+                MessageBox.Show(EX.ToString());
+                throw;
+            }
+            SQLControl.cnn.Close();
+            txtnombre.Clear();
+            txtusuario.Clear();
+            txtpass.Clear();
+        }
+
+        public void ActualizarUsuario()
+        {
+            SQLControl.cnn.Open();
+            SqlCommand cmd = new SqlCommand("spActualizarUsuario", SQLControl.cnn);
+            cmd.CommandType= CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@usuario", txtusuario.Text);
+            cmd.Parameters.AddWithValue("@nombre", txtnombre.Text);
+            cmd.Parameters.AddWithValue("@pass", txtpass.Text);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Usuario actualizado correctamente.");
+            }
+            catch (SqlException EX)
+            {
+                MessageBox.Show(EX.ToString());
+                throw;
+            }
+            SQLControl.cnn.Close();
+            txtnombre.Clear();
+            txtusuario.Clear();
+            txtpass.Clear();
+        }
+
+        public void EliminarUsuario()
+        {
+            SQLControl.cnn.Open();
+            SqlCommand cmd = new SqlCommand("spEliminarUsuario",SQLControl.cnn);
+            cmd.CommandType=CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@usuario",txtusuario.Text);
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Usuario eliminado correctamente.");
+            }
+            catch (SqlException EX)
+            {
+                MessageBox.Show(EX.ToString());
+                throw;
+            }
+            SQLControl.cnn.Close();
+            txtnombre.Clear();
+            txtusuario.Clear();
+            txtpass.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AgregarUsuario();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            ActualizarUsuario();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            EliminarUsuario();
+        }
+    }
+}
